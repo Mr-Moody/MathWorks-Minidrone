908a1e2ca9e2c78f6272810113813c6189c8eb1c
Mr-Moody <118393511+Mr-Moody@users.noreply.github.com>
2025-10-05 20:49:36 +0100

    Tuned more and added feedforward

--- weightingCalculation.m
+++ weightingCalculation.m
@@ -1,22 +1,21 @@
 n = 120;
 m = 160;
 
-weighting = zeros(n, m/2);
+temp = zeros(n, m/2);
 
-    for i=1:n
-        for j=1:m
-            weighting(i,j) = sqrt(i^2+j^2);
-        end
+for i=1:n
+    for j=1:m/2
+        temp(i,j) = sqrt(i^2+j^2);
     end
+end
 
-    weighting = weighting / 200;
+temp = temp / 200;
 
-    weighting = rot90(weighting, 2);
+temp = rot90(temp, 2);
 
-    % weighting = cat(1,weighting,flip(weighting,2));
+% weighting = [temp flip(temp,2)];
+weighting = [flip(temp,2) temp];
 
-    weighting = [weighting flip(weighting,2)];
+weighting = weighting .^ 3;
 
-  
-
-    display(weighting)
+heatmap(weighting);
