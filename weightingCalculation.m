d0b9063d0bd6662f4797711e70297de5fd8cad9a
Mr-Moody <118393511+Mr-Moody@users.noreply.github.com>
2025-10-10 20:47:53 +0100

    Current State

--- weightingCalculation.m
+++ weightingCalculation.m
@@ -1,5 +1,8 @@
+%% Variables
 n = 120;
 m = 160;
+
+%% Weighting 1
 
 temp = zeros(n, m/2);
 
@@ -13,9 +16,32 @@ temp = temp / 200;
 
 temp = rot90(temp, 2);
 
-% weighting = [temp flip(temp,2)];
-weighting = [flip(temp,2) temp];
+weighting = [temp flip(temp,2)];
 
-weighting = weighting .^ 3;
+weighting = weighting .^ 0.7;
+
+weighting(:,1:3) = weighting(:,1:3) * 5;
+weighting(:,m-2:m) = weighting(:,m-2:m) * 5;
+
+heatmap(weighting);
+
+%% Weighting 2
+
+temp = zeros(n, m/2);
+
+for i=1:n
+    for j=1:m/2
+        temp(i,j) = j;
+    end
+end
+
+temp = temp / 200;
+
+temp = rot90(temp, 2);
+
+weighting = [temp flip(temp,2)];
+
+weighting(:,1:3) = 10;
+weighting(:,m-2:m) = 10;
 
 heatmap(weighting);
